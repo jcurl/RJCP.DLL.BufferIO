41b18752abe87382b2bcdffa8fdda12a524473b8
Jason Curl <jcurl@arcor.de>
2021-09-27 12:04:57 +0200

    TimerExpiry: Use Environment.TickCount instead of stopwatch
    The Environment.TickCount is less overhead than a stopwatch and has 1ms accuracy which is good enough for the use cases for TimerExpiry.
    
    Issue: DOTNET-421

--- BufferIO/Timer/TimerExpiry.cs
+++ BufferIO/Timer/TimerExpiry.cs
@@ -1,7 +1,6 @@
 ﻿namespace RJCP.IO.Timer
 {
-    using System.Diagnostics;
-    using System.Threading;
+    using System;
 
     /// <summary>
     /// A class to maintain how much time is remaining since the last reset, until expiry.
@@ -10,11 +9,11 @@
     /// This class is useful when implementing time outs in other methods. It can provide the remaining time, in units
     /// of milliseconds, that can be used with many Operating System calls as an expiry time.
     /// <para>
-    /// One example is the <see cref="WaitHandle.WaitOne()"/> method which expects a time out parameter. Either
-    /// instantiate the <see cref="TimerExpiry"/> class at the beginning immediately before its use, or call the
-    /// <see cref="Reset"/> method at the beginning of the time out operation. Then on return of the function, if no
-    /// other operation occurred, the method <see cref="RemainingTime"/> should return 0 indicating that the timer has
-    /// expired.
+    /// One example is the <see cref="System.Threading.WaitHandle.WaitOne()"/> method which expects a time out
+    /// parameter. Either instantiate the <see cref="TimerExpiry"/> class at the beginning immediately before its use,
+    /// or call the <see cref="Reset"/> method at the beginning of the time out operation. Then on return of the
+    /// function, if no other operation occurred, the method <see cref="RemainingTime"/> should return 0 indicating that
+    /// the timer has expired.
     /// </para>
     /// <para>
     /// Another thread can be programmed to <see cref="Reset"/> the timer class during a time out operation, so that
@@ -52,37 +51,48 @@
     /// </remarks>
     public sealed class TimerExpiry
     {
-        private readonly Stopwatch m_StopWatch = new Stopwatch();
-        private int m_Milliseconds;
+        private int m_StartTime;
+        private int m_Duration;
+        private bool m_Expired;
 
         /// <summary>
         /// Constructor. Initialise expiry based on the current time.
         /// </summary>
         /// <param name="milliseconds">The initial time out in milliseconds.</param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// The value of <paramref name="milliseconds"/> is not a valid value.
+        /// </exception>
         /// <remarks>
         /// The constructor sets the initial time out that should be used. On construction of the new object, the timer
         /// is automatically started.
         /// </remarks>
         public TimerExpiry(int milliseconds)
         {
-            Timeout = milliseconds;
+            if (milliseconds < System.Threading.Timeout.Infinite)
+                throw new ArgumentOutOfRangeException(nameof(milliseconds));
+            m_Duration = milliseconds;
+            m_StartTime = Environment.TickCount;
         }
 
         /// <summary>
-        /// The time for expiry on the next reset. <see cref="Timeout.Infinite"/> indicates no expiry.
+        /// The time for expiry on the next reset.
         /// </summary>
+        /// <value>
+        /// The expiry timeout in milliseconds. The value <see cref="System.Threading.Timeout.Infinite"/> indicates no expiry.
+        /// </value>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// The value of <see name="Timeout"/> is not a valid value.
+        /// </exception>
         public int Timeout
         {
-            get { return m_Milliseconds; }
+            get { return m_Duration; }
             set
             {
-                m_StopWatch.Reset();
-                if (value < 0) {
-                    m_Milliseconds = System.Threading.Timeout.Infinite;
-                } else {
-                    m_Milliseconds = value;
-                    if (value > 0) m_StopWatch.Start();
-                }
+                if (value < System.Threading.Timeout.Infinite)
+                    throw new ArgumentOutOfRangeException(nameof(value));
+                m_Expired = false;
+                m_Duration = value;
+                m_StartTime = Environment.TickCount;
             }
         }
 
@@ -92,11 +102,17 @@
         /// <returns>The time to expiry in milliseconds.</returns>
         public int RemainingTime()
         {
-            if (m_Milliseconds < 0) return System.Threading.Timeout.Infinite;
+            if (m_Expired) return 0;
+            if (m_Duration == System.Threading.Timeout.Infinite)
+                return System.Threading.Timeout.Infinite;
 
-            long elapsed = m_StopWatch.ElapsedMilliseconds;
-            if (elapsed >= m_Milliseconds) return 0;
-            return (int)(m_Milliseconds - elapsed);
+            // Unchecked, if the Environment.TickCount has exceeded 31 bits, we don't care.
+            int remaining = unchecked(m_StartTime + m_Duration - Environment.TickCount);
+            if (remaining < 0) {
+                m_Expired = true;
+                return 0;
+            }
+            return remaining;
         }
 
         /// <summary>
@@ -112,10 +128,9 @@
         /// </summary>
         public void Reset()
         {
-            if (m_Milliseconds > 0) {
-                m_StopWatch.Reset();
-                m_StopWatch.Start();
-            }
+            if (m_Duration == System.Threading.Timeout.Infinite) return;
+            m_Expired = false;
+            m_StartTime = Environment.TickCount;
         }
     }
 }
--- BufferIOTest/Timer/TimerExpiryTest.cs
+++ BufferIOTest/Timer/TimerExpiryTest.cs
@@ -8,6 +8,7 @@
     public class TimerExpiryTest
     {
         [Test]
+        [Timeout(1000)]
         public void TimerExpiry_Basic()
         {
             TimerExpiry te = new TimerExpiry(200);
@@ -29,57 +30,66 @@
                 re = te.RemainingTime();
             } while (re > 0);
             Assert.That(te.RemainingTime(), Is.EqualTo(0));
+            Assert.That(te.Expired, Is.True);
 
             for (int i = 0; i < c; i++) {
                 Console.WriteLine("Wait {0}: {1}", i, rea[i]);
             }
+
+            // Ensure that resetting will reenact the timer.
+            te.Reset();
+            Assert.That(te.Expired, Is.False);
+            Assert.That(t, Is.GreaterThan(140));
         }
 
         [Test]
+        [Timeout(5000)]
         public void TimerExpiry_Reset()
         {
             TimerExpiry te = new TimerExpiry(1000);
-            Thread.Sleep(350);
-            Assert.That(te.RemainingTime(), Is.GreaterThan(500));
+            Thread.Sleep(250);
+            Assert.That(te.RemainingTime(), Is.GreaterThan(600));
             te.Reset();
 
-            Thread.Sleep(350);
-            Assert.That(te.RemainingTime(), Is.GreaterThan(500));
+            Thread.Sleep(250);
+            Assert.That(te.RemainingTime(), Is.GreaterThan(600));
             te.Reset();
 
-            Thread.Sleep(350);
-            Assert.That(te.RemainingTime(), Is.GreaterThan(500));
+            Thread.Sleep(250);
+            Assert.That(te.RemainingTime(), Is.GreaterThan(600));
             te.Reset();
 
-            Thread.Sleep(350);
-            Assert.That(te.RemainingTime(), Is.GreaterThan(500));
+            Thread.Sleep(250);
+            Assert.That(te.RemainingTime(), Is.GreaterThan(600));
             te.Reset();
 
-            Thread.Sleep(350);
-            Assert.That(te.RemainingTime(), Is.GreaterThan(500));
+            Thread.Sleep(250);
+            Assert.That(te.RemainingTime(), Is.GreaterThan(600));
             te.Reset();
 
-            Thread.Sleep(350);
-            Assert.That(te.RemainingTime(), Is.GreaterThan(500));
+            Thread.Sleep(250);
+            Assert.That(te.RemainingTime(), Is.GreaterThan(600));
         }
 
         [Test]
+        [Timeout(5000)]
         public void TimerExpiry_Reset2()
         {
             TimerExpiry te = new TimerExpiry(1000);
-            Thread.Sleep(350);
-            Assert.That(te.RemainingTime(), Is.GreaterThan(500));
-            Thread.Sleep(350);
-            Assert.That(te.RemainingTime(), Is.LessThan(500));
+            Thread.Sleep(250);
+            Assert.That(te.RemainingTime(), Is.GreaterThan(600));
+            Thread.Sleep(250);
+            Assert.That(te.RemainingTime(), Is.LessThan(550));
 
             te.Reset();
-            Thread.Sleep(350);
-            Assert.That(te.RemainingTime(), Is.GreaterThan(500));
-            Thread.Sleep(350);
-            Assert.That(te.RemainingTime(), Is.LessThan(500));
+            Thread.Sleep(250);
+            Assert.That(te.RemainingTime(), Is.GreaterThan(600));
+            Thread.Sleep(250);
+            Assert.That(te.RemainingTime(), Is.LessThan(550));
         }
 
         [Test]
+        [Timeout(2000)]
         public void TimerExpiry_Negative()
         {
             TimerExpiry te = new TimerExpiry(-1);
@@ -91,7 +101,7 @@
             Assert.That(te2.RemainingTime(), Is.GreaterThan(0));
             te2.Timeout = -1;
             Assert.That(te.RemainingTime(), Is.EqualTo(Timeout.Infinite));
-            Thread.Sleep(400);
+            Thread.Sleep(300);
             Assert.That(te.RemainingTime(), Is.EqualTo(Timeout.Infinite));
         }
 
@@ -101,6 +111,48 @@
             TimerExpiry te = new TimerExpiry(0);
             Assert.That(te.Expired, Is.True);
             Assert.That(te.RemainingTime(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void TimerExpiry_InvalidValue()
+        {
+            Assert.That(() => { _ = new TimerExpiry(-2); },
+                Throws.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        public void TimerExpiry_TimeoutInvalidValue()
+        {
+            TimerExpiry te = new TimerExpiry(200);
+            Assert.That(te.Expired, Is.False);
+            Assert.That(te.Timeout, Is.EqualTo(200));
+            Assert.That(te.RemainingTime(), Is.GreaterThan(0).And.LessThanOrEqualTo(200));
+
+            Assert.That(() => { te.Timeout = -2; },
+                Throws.TypeOf<ArgumentOutOfRangeException>());
+
+            Assert.That(te.Expired, Is.False);
+            Assert.That(te.Timeout, Is.EqualTo(200));
+            Assert.That(te.RemainingTime(), Is.GreaterThan(0).And.LessThanOrEqualTo(200));
+        }
+
+        [Test]
+        public void TimerExpiry_LargeTimeout()
+        {
+            TimerExpiry te = new TimerExpiry(0x7FFFFFFF);
+            Assert.That(te.Expired, Is.False);
+            Assert.That(te.Timeout, Is.EqualTo(0x7FFFFFFF));
+            Assert.That(te.RemainingTime(), Is.GreaterThan(0x7FFFFFFF - 100).And.LessThanOrEqualTo(0x7FFFFFFF));
+
+            int start = Environment.TickCount;
+            Thread.Sleep(100);
+            int end = Environment.TickCount;
+            int elapsed = unchecked(end - start);
+            Assert.That(te.Expired, Is.False);
+            Assert.That(te.Timeout, Is.EqualTo(0x7FFFFFFF));
+            Assert.That(te.RemainingTime(), Is.GreaterThan(0x7FFFFFFF - 200).And.LessThanOrEqualTo(0x7FFFFFFF - elapsed));
+
+            Console.WriteLine($"Elapsed {elapsed}, TimerExpiry {unchecked(te.Timeout - te.RemainingTime())}");
         }
     }
 }
